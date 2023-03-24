9113cffd3c00f564ba9fcf9390ff2bef28f83063
Jeff <doylejeff98@gmail.com>
2023-03-24 10:38:26 -0400

    create home const added in home.js

---
+++ src/home.js
@@ -0,0 +1,12 @@
+const createHome = (()  => {
+    
+    const contentContainer = document.querySelector("#content");
+
+
+    const home = document.createElement("div");
+    home.classList.add("tab");
+
+    home.innerHTML = ``;
+  
+
+  })();
